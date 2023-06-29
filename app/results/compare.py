4973e05361cb84d2353bd4ba99bb60772662fa0d
cyxteen <49212444+Cyxteen@users.noreply.github.com>
2023-06-29 14:06:35 +0300

    minor changes

---
+++ app/results/compare.py
@@ -0,0 +1,7 @@
+# imports
+from .threading import process_multiple_years
+
+# compares different schools
+def compareSchools(school_one_name: str, school_two_name: str, level: str, year: int):
+    
+    pass
