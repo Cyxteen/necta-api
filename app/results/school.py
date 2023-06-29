7ebf386011b213abe728826bd6ad781046376a06
cyxteen <49212444+Cyxteen@users.noreply.github.com>
2023-06-29 17:58:03 +0300

    minor changes to school.py

--- app/results/school.py
+++ app/results/school.py
@@ -12,8 +12,9 @@ def compare(name_of_school: str, type_of_exam: str, start_year: int, end_year=0)
     name_of_school = name_of_school.lower()
     type_of_exam = type_of_exam.lower()
 
+    diff_years = end_year - start_year
     # checks if only oney year is passed
-    if end_year == 0:
+    if end_year == 0 and diff_years == 0:
         single_year_results = single_year(name_of_school, type_of_exam, start_year, curr_path)
         return single_year_results
     
